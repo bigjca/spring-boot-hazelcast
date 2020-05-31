c8f84aca405be4cb136a2962c6905d28c4ab79c5
John Adler <bigjca@gmail.com>
2020-05-31 17:03:37 -0400

    kubernetes cache config

--- src/main/java/bigjca/sandbox/CacheConfig.java
+++ src/main/java/bigjca/sandbox/CacheConfig.java
@@ -1,5 +1,6 @@
 package bigjca.sandbox;
 
+import com.hazelcast.config.Config;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.spring.cache.HazelcastCacheManager;
 import org.springframework.cache.CacheManager;
@@ -10,6 +11,16 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableCaching
 public class CacheConfig {
+    @Bean
+    Config config() {
+        Config config = new Config();
+        config.getNetworkConfig().getJoin().getMulticastConfig().setEnabled(false);
+        config.getNetworkConfig().getJoin().getKubernetesConfig().setEnabled(true)
+                .setProperty("namespace", "jsandbox")
+                .setProperty("service-name", "hazelcast-service");
+        return config;
+    }
+
     @Bean
     CacheManager cacheManager() {
         return new HazelcastCacheManager(Hazelcast.newHazelcastInstance());
